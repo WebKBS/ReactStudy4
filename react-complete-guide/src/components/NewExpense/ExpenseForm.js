2d3a053c27fc028d4adf060ff3023a96d5fe99b2
BeomSu-Kang <kbs21c1987@gmail.com>
2022-10-02 18:11:38 +0900

    useState, onClick으로 화면 받고 열기

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -92,6 +92,10 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        {/* 프롭스로 onCancel 전달받음 */}
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
