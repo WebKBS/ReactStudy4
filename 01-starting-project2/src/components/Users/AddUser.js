08f2468c7750c1136fe3e6727f87139f8d71a7b7
BeomSu-Kang <kbs21c1987@gmail.com>
2022-12-18 19:05:04 +0900

    Ui 만들기

--- 01-starting-project2/src/components/Users/AddUser.js
+++ 01-starting-project2/src/components/Users/AddUser.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -6,13 +9,16 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="text" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      {/* 스타일적용 시키려면 props로 전달해야한다. */}
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="text" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
